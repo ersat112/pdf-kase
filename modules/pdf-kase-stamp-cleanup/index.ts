5e5e6b58a588ae99fcff989ef19cd5e7e989a221
ersat112 <ersat.bulbul@gmail.com>
2026-03-21 22:00:20 +0300

    Delete modules/pdf-kase-stamp-cleanup directory

--- modules/pdf-kase-stamp-cleanup/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport the native module. On web, it will be resolved to PdfKaseStampCleanupModule.web.ts
-// and on native platforms to PdfKaseStampCleanupModule.ts
-export { default } from './src/PdfKaseStampCleanupModule';
-export { default as PdfKaseStampCleanupView } from './src/PdfKaseStampCleanupView';
-export * from  './src/PdfKaseStampCleanup.types';
